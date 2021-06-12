fdcbe52c7dfdab738d1c80d382d17c45c625591f
선우 나 <nsun527@naver.com>
2021-06-12 13:23:11 +0900

    Android start...

---
+++ TimeTableMobile/TimeTableMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TimeTableMobile.ViewModels;
+using Xamarin.Forms;
+
+namespace TimeTableMobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ TimeTableMobile/TimeTableMobile/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTableMobile.Models;
+using TimeTableMobile.ViewModels;
+using TimeTableMobile.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TimeTableMobile.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
